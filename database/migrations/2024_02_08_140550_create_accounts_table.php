b79ef5503b2c2193b53b39ccc659cec291ddfebc
nemanja1991 <nemanja.jovanovic91@live.com>
2024-02-08 15:16:14 +0100

    jwt implementation

---
+++ database/migrations/2024_02_08_140550_create_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class, 'user_id');
+            $table->string('account_name');
+            $table->string('website_url')->nullable();
+            $table->string('username')->nullable();
+            $table->string('password');
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
