7f70c8a130ee9d6516b4a520cff9d07c457c04c3
nemanja1991 <nemanja.jovanovic91@live.com>
2024-02-09 16:08:41 +0100

    test3

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => ['http://127.0.0.1:8000/'],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
